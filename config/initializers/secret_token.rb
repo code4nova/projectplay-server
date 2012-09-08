da8734eccb029ab0e48d2ba35601721556d9bd36
Michelle Koeth <michelle@codeforamerica.org>
2012-09-08 12:51:10 -0400

    Basic Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectplay::Application.config.secret_token = '6fccf660fdf39785df8a34923ced3269da92e773bf400a5c0435f787e87337725f697637e2c3966d971793b7f0d7156a3ae4717d02a4ede9eb5e56feac94478e'
