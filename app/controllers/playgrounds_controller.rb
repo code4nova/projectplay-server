e0278bf08ab4b7662137a74f5abb32609c6f6719
Michelle Koeth <michelle@codeforamerica.org>
2012-09-09 15:31:27 -0400

    addToGooglePlaces function to test Places API access - so far it adds the first playground - more work to be done later

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -1,3 +1,4 @@
+require 'net/http'
 class PlaygroundsController < ApplicationController
   # GET /playgrounds
   # GET /playgrounds.json
@@ -8,6 +9,51 @@ class PlaygroundsController < ApplicationController
       format.html # index.html.erb
       format.json { render :json => @playgrounds }
     end
+  end
+
+  def addToGooglePlaces
+    # The API key info is stored in an environmental variable for privacy
+    # To add env variables to Heroku, use for ex: heroku config:add GPLACES_KEY=8N029N81
+    @playgrounds = Playground.all
+    url = "https://maps.googleapis.com/maps/api/place/add/json?sensor=false&key=" + ENV['GPLACES_KEY']
+    #@playgrounds.each do |p|
+    p = @playgrounds.first
+    @playground = p
+      # Form the json request according to Google Places API:
+      # https://developers.google.com/places/documentation/actions#PlaceReports
+      json =  { "location" => { "lat" => p.lat, "lng" => p.long}, "accuracy" => 50, "name" => p.name, "types" => ["park"], "language" => "en" }.to_json
+      @returninfo = post(url,json)
+      @jsonrequest = json
+    #end
+    # @returninfo will be available to the view 
+    
+    respond_to do |format|
+      format.html # addToGooglePlaces.html.erb
+      format.json { render :json => @playgrounds }
+    end
+  end
+=begin
+  def requestgp(req)
+    res = Net::HTTP.start('maps.googleapis.com', '443') { |http|http.request(req) }
+    unless res.kind_of?(Net::HTTPSuccess)
+      handle_error(req, res)
+    end
+    res
+  end
+=end
+  def post(uri, json)
+    # this sequence of code must be done for HTTP/SSL/Basic Auth b/c use_ssl must be specified before starting session
+    res = ""
+    http = Net::HTTP.new('maps.googleapis.com', '443')
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    resp = http.start do |ht|  
+      req = Net::HTTP::Post.new(uri)
+      req["content-type"] = "application/json"
+      req.body = json
+      res = ht.request(req)
+    end
+    res.body
   end
 
   # GET /playgrounds/1
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Projectplay::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'playgrounds#index'
 
-  resources :playgrounds
+  resources :playgrounds do
+    collection do
+      get :addToGooglePlaces
+    end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,9 +60,7 @@ Projectplay::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'playgrounds#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
