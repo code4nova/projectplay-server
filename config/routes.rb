b6cad9a74a6f91b60d8a67d70d106d3328745ddd
Michelle Koeth <michelle@codeforamerica.org>
2012-09-09 02:45:01 -0400

    Ribbon styling

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Projectplay::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'playground#index'
+  root :to => 'playgrounds#index'
 
   # See how all your routes lay out with "rake routes"
 
