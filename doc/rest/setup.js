80ef5058e7d0e73d618cc20758a05ee92bdb1921
lastlink <theefunk@gmail.com>
2018-06-21 21:02:31 -0400

    document rest endpoint

---
+++ doc/rest/setup.js
@@ -0,0 +1,23 @@
+// Do command + , and paste these settings there
+var settings = {
+	"rest-client.environmentVariables": {
+		"pp-dev": {
+			"protocol": "http",
+			"host": "localhost",
+			"port": ":3000",
+			"endpoint": ""
+		},
+		"pp-py-live": {
+			"protocol": "http",
+			"host": "jerseycoder.webfactional.com",
+			"port": "",
+			"endpoint": ""
+		},
+		"pp-wp-live": {
+			"protocol": "http",
+			"host": "jerseycoder.webfactional.com",
+			"port": "",
+			"endpoint": ""
+		}
+	}
+}
