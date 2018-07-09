d8d275f7c9ef1abf09bc2bd3d356d6e157a8a200
lastlink <theefunk@gmail.com>
2018-07-09 14:20:54 -0400

    finished seeder

--- py-app/seeder.py
+++ py-app/seeder.py
@@ -4,9 +4,11 @@ import pandas
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///cdb.db')
+engine = create_engine('sqlite:///./app/development.sqlite3')
 Base.metadata.create_all(engine)
 
-file_name = 'SeedData12_2013.csv'
-df = pandas.read_csv(file_name)
-df.to_sql(con=engine, index_label='id', name="playgrounds", if_exists='replace')
+file_name = '../db/SeedData12_2013.csv'
+dateCols = ['Created_at', 'Updated_at']
+df = pandas.read_csv(file_name, parse_dates=dateCols)
+
+df.to_sql(con=engine, name="playgrounds", if_exists='replace')
