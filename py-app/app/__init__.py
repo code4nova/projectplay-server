e63330ab8123c221d8f1100ddb7ae9079b70764b
lastlink <theefunk@gmail.com>
2018-07-09 13:36:21 -0400

    get rid of some junk

--- py-app/app/__init__.py
+++ py-app/app/__init__.py
@@ -9,23 +9,9 @@ import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'development.sqlite3')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# import os
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'super secret key'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/mydatabase.db'
-# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
-# db = SQLAlchemy(app)
-
-# app.config.from_object(__name__)
 from app import views
 
 
